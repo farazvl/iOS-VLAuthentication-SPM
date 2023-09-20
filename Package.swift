8bca77840cbb7aff35468b3c311e376c9f2a8392
farazvl <farazhabib@viewlift.com>
2023-09-20 19:14:13 +0530

    Removed target dependecies

--- Package.swift
+++ Package.swift
@@ -27,8 +27,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VLAuthentication",
-            dependencies: [.product(name: "VLBeaconLib", package: "VLBeaconSwift")]),
+            name: "VLAuthentication"),
         .testTarget(
             name: "VLAuthenticationTests",
             dependencies: ["VLAuthentication"]),
