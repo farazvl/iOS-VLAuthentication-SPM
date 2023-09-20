cb41dcdd1db4ec1d0e88fc9ea154a73ef113ff13
farazvl <farazhabib@viewlift.com>
2023-09-20 18:08:36 +0530

    Added dependencies to target

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VLAuthentication"),
+            name: "VLAuthentication", dependencies: [.product(name: "VLBeaconLib", package: "VLBeaconSwift")]),
         .testTarget(
             name: "VLAuthenticationTests",
             dependencies: ["VLAuthentication"]),
